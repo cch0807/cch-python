d9e3024f4f185dc920929bb9075434f67207a9c9
chchoi <ch.choi@aizen.co>
2022-09-05 17:12:24 +0900

    20220905_2

--- First Class func 5.py
+++ First Class func 5.py
@@ -42,7 +42,7 @@ print(isinstance(t, abc.Iterable))
 print()
 print()
 
-# next
+# next 패턴
 class WordSplitter:
     def __init__(self, text):
         self._idx = 0
@@ -63,3 +63,49 @@ class WordSplitter:
 wi = WordSplitter('Do Today What you could do tommorrow')
 
 print(wi)
+
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+print(next(wi))
+# print(next(wi))
+
+print()
+print()
+
+# Generator 패턴
+# 1. 지능형 리스트, 딕셔너리, 집합 -> 데이터 양 증가 후 메모리 사용량 증가-> 제네레이터 사용 권장
+# 2. 단위 실행 가능한 코루틴(corutine) 구현과 연동
+# 3. 작은 메모리 조각 사용
+
+class WordSplitGenerator:
+    def __init__(self, text):
+        self._text = text.split(' ')
+    
+    def __iter__(self):
+        for word in self._text:
+            yield word # 제네레이터
+        return
+    
+    def __repr__(self):
+        return 'WordSplitGenerator(%s)' % (self._text)
+
+wg = WordSplitGenerator('Do today what you could do tommorrow')
+
+wt = iter(wg)
+
+print(wt, wg)
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+# print(next(wt))
+
+print()
+print()
