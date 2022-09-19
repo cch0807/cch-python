4083305a0f8d28e235ef64ad832e46928dd77a1f
cch0807 <ch.choi@aizen.co>
2022-09-19 16:22:03 +0900

    20220919_4

--- Concurrency 3.py
+++ Concurrency 3.py
@@ -4,13 +4,14 @@
 
 # 코루틴 : 단일(싱글) 스레드, 스택을 기반으로 동작하는 비동기 작업
 # 스레드 : OS 관리, CPU 코어에서 실시간, 시분할 비동기 작업 -> 멀티 스레드
-# yield : 메인 <-> 서브
+# yield, send: 메인 <-> 서브
 # 코루틴 제어, 상태, 양방향 전송
 
 # 서브루틴, 메인루틴 호출 -> 서브루틴에서 수행(흐름 제어)
 # 코루틴 : 루틴 실행 중 중지 (시점 기억) -> 동시성 프로그래밍 
 # 코루틴 : 스레드에 비해 오버헤드 감소
 # 스레드 : 싱글스레드 -> 멀티스레드 -> 복잡 -> 공유되는 자원 -> 교착 상태 발생 가능성, 컨텍스트 스위칭 비용이 발생, 자원 소비 가능성 증가
+# 3.5 이상 def -> async , yield -> await
 
 # 코루틴 Ex1
 
@@ -52,13 +53,56 @@ def coroutine2(x):
     print('>>> coroutine received : {}'.format(y))
     z = yield x + y
     print('>>> coroutine received : {}'.format(z))
+    t = yield
 
 cr3 = coroutine2(10)
 
 from inspect import getgeneratorstate
 
-print(getgeneratorstate)
+print(getgeneratorstate(cr3))
 
 print(next(cr3))
+
 cr3.send(100)
-cr3.send(100)
+
+print()
+print()
+
+# 코루틴 Ex3
+# StopIteration 자동 처리(3.5 -> await)
+# 중첩 코루틴 처리
+
+def generator1():
+    for x in 'AB':
+        yield x
+    
+    for y in range(1,4):
+        yield y
+
+t1 =generator1()
+
+print(next(t1))
+print(next(t1))
+print(next(t1))
+print(next(t1))
+print(next(t1))
+
+t2 = generator1()
+
+print(list(t2))
+
+print()
+print()
+
+def generator2():
+    yield from 'AB'
+    yield from range(1,4)
+
+t3 = generator2()
+
+print(next(t3))
+print(next(t3))
+print(next(t3))
+print(next(t3))
+print(next(t3))
+
