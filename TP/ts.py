abee00d35a7ff5e05e7f3329a2ddd2178919e471
cch0807 <ch.choi@aizen.co>
2022-09-28 10:12:17 +0900

    20220928_1

--- TP/ts.py
+++ TP/ts.py
@@ -3,6 +3,9 @@
 # test_ro = Robot("test", "1234")
 
 
+from re import S
+
+
 class Robot:
     test_num = 0
 
@@ -10,6 +13,7 @@ class Robot:
         self.name = "asdf"
         self.code = 1235
         self.test_num = 0
+        self.test = test()
 
     def ad(self):
         test = test_Robot(1234)
@@ -17,11 +21,16 @@ class Robot:
         print(self.test_num)
 
     def tests(self):
-        print(self.name, self.code)
-        res = test(self.name, self.code)
-        self.name = res[0]
-        self.code = res[1]
-        print(self.name, self.code)
+        # print(self.name, self.code)
+        # res = test(self.name, self.code)
+        # self.name = res[0]
+        # self.code = res[1]
+        # print(self.name, self.code)
+        # print(self.name2)
+        # print(self.code2)
+        print(self.test)
+        for i in self.test:
+            print(i)
 
 
 class test_Robot:
@@ -36,12 +45,30 @@ class test_Robot:
         return self.test_num
 
 
-def test(name, code):
-    name = name
-    code = code
-    name = "fdas"
-    code = 6546
-    return (name, code)
+def test():
+    name2 = "test"
+    code2 = 1234
+    return (name2, code2)
 
 
 test = Robot().tests()
+
+
+class ParentEx2:
+    def __init__(self):
+        self.value = 5
+
+    # def get_value(self):
+    #     return self
+
+
+class ChildEx2(ParentEx2):
+    def get_value(self):
+        return self
+
+    def sum_value(self):
+        test = self.get_value()
+        print(test.value)
+
+
+c = ChildEx2().sum_value()
