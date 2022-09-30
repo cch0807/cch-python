03c32b02f4f00fbc5d793550cb2b10cfc83394db
cch0807 <ch.choi@aizen.co>
2022-09-30 18:03:06 +0900

    20220930_3

--- cch_git.sh
+++ cch_git.sh
@@ -2,7 +2,24 @@
 dt=$(date +'%Y%m%d_%s')
 
 git add .
-git commit -m $dt
+res= git status
+
+$res
+
+
+# if [ "${res}"==*"modified:"* ]; then
+#     echo 'warning'
+# else
+#     echo 'fiald'
+# fi
+
+echo 'Enter the commit message'
+read commit_message
+
+git commit -m $commit_message
+
+
 git push origin mac
+git status
 
 echo success upload
