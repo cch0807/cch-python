ea0617e033d5eaaac5f660f27e717d10257f6aef
choich0807 <ch.choi@aizen.co>
2022-12-13 15:58:48 +0900

    20221213_4

--- concurrency/io-coroutine.py
+++ concurrency/io-coroutine.py
@@ -1,38 +1,11 @@
-# pip install requests
-
-# Sync
-
-import requests
-import time
-
-def fetcher(session, url):
-    with session.get(url) as response:
-        return response.text
-
-def main():
-    urls = ["https://naver.com", "https://google.com", "https://instagram.com"] * 3
-
-    # session = requests.Session()
-    # session.get(url)
-    # session.close()
-
-    with requests.Session() as session:
-        result = [fetcher(session, url) for url in urls]
-        print(result)
-
-if __name__ == "__main__":
-    start = time.time()
-    # main()
-    end = time.time()
-    print(end - start)
-
-
-# Async
-
 import aiohttp
 import asyncio
+import os
+import threading
+import time
 
 async def fetcher(session, url):
+    print(f"{os.getpid()} process | {threading.get_ident()} url : {url}")
     async with session.get(url) as response:
         return await response.text()
 
@@ -48,12 +21,10 @@ async def main():
         # print(result)
         result = await asyncio.gather(*[fetcher(session, url) for url in urls])
         # result = await fetcher(session, urls[0])
-        print(result)
+        # print(result)
 
 if __name__ == "__main__":
     start = time.time()
     asyncio.run(main())
     end = time.time()
     print(end - start)
-
-
