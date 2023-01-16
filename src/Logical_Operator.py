8b57ad9a9df2b033aef149e94a2977ce82a9affe
cch0807 <ckdgus11021@gmail.com>
2023-01-16 23:32:10 +0900

    20230116

---
+++ src/Logical_Operator.py
@@ -0,0 +1,13 @@
+# Logical Operators(논리 연산자)
+# A and B : Both should be True
+# X or Y : One of them is True
+# not E : The opposite value True <-> False
+
+value = 10
+if isinstance(value, int) and value > 5:
+    print("Correct")
+else:
+    print("Not correct")
+
+# if not isinstance(value, float):
+# print(f"{value} is not float)
---
+++ src/rand0m.py
@@ -0,0 +1,10 @@
+# random module
+import random
+
+print(random.randint(1, 100))
+
+random_float = random.random()
+# print(random_float)
+
+# how to generate 0 - 10???
+print(int(random_float * 10))
