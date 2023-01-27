eb85c4fdcbc6d8a9d9d39c36656a38b2a1678bac
cch0807 <ckdgus11021@gmail.com>
2023-01-27 23:34:57 +0900

    20230127_1

--- src/part2/reverse_string.py
+++ src/part2/reverse_string.py
@@ -9,3 +9,17 @@ print("".join(value_list))
 
 # 2 reversed
 print("".join(list(reversed(value))))
+
+# 3 forloop
+temp_list = []
+for i in range(len(value) - 1, -1, -1):
+    temp_list.append(value[i])
+print(temp_list)
+print("".join(temp_list))
+
+# 4 while
+value_list = list(value)
+temp_list = []
+while len(value_list) > 0:
+    temp_list.append(value_list.pop())
+print("".join(temp_list))
