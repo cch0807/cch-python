5b59f74ee0f247409360dfe195455fd6b200167e
cch0807 <ckdgus11021@gmail.com>
2023-02-06 23:47:51 +0900

    20230206_1

--- src/part3/assignment_operator.py
+++ src/part3/assignment_operator.py
@@ -22,4 +22,13 @@ print(b)
 print(a)
 
 # # Deep Copy
-# # A deep copy constructs a new compound
+# # A deep copy constructs a new compound object and then, recursively,
+# inserts copies into it of the objects found in the original.
+
+a = [[1, 2], [2, 4]]
+import copy
+
+b = copy.deepcopy(a)  ## deep copy
+b[0].append(4)
+print(b)
+print(a)
